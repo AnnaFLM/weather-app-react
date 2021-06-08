45576837bf49e2ce451b0f849a86d1ebb87cd810
Anna Mehnert <mehnert.anna@gmail.com>
2021-06-08 19:32:18 +0200

    added new component WeatherForecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 import axios from "axios";
 
@@ -57,6 +58,7 @@ export default function Weather(props) {
           />
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
