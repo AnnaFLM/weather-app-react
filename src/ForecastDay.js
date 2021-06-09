056e377fccfa98f82243f3d80c0567f2222b8e74
Anna Mehnert <mehnert.anna@gmail.com>
2021-06-09 08:20:15 +0200

    created new component ForecastDay

---
+++ src/ForecastDay.js
@@ -0,0 +1,34 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function ForecastDay(props) {
+  function temperatureMax() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+
+  function temperatureMin() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Mon", "Tues", "Wed", "Thu", "Fri"];
+
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="day">{day()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
+      <div className="props.data-temperature">
+        <span className="props.data-tempMax">{temperatureMax()}</span>{" "}
+        <span className="props.data-tempMin">{temperatureMin()}</span>
+      </div>
+    </div>
+  );
+}
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
+import ForecastDay from "./ForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
@@ -18,12 +18,7 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           <div className="col">
-            <div className="day">Wed</div>
-            <WeatherIcon code="01d" size={36} />
-            <div className="forecast-temperature">
-              <span className="forecast-tempMax">{forecast[0].temp.max}</span>{" "}
-              <span className="forecast-tempMin">{forecast[0].temp.min}</span>
-            </div>
+            <ForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
@@ -32,7 +27,7 @@ export default function WeatherForecast(props) {
     let apiKey = "c383ea970c0b6605f15ecef4e8d3a578";
     let latitude = props.coordinates.lat;
     let longitude = props.coordinates.lon;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,daily&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
 
